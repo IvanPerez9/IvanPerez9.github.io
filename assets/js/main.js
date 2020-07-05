66f7259e1c37bce18bec4a13d123857541c95017
Iván Pérez Huete <ivan120695@gmail.com>
2020-07-05 10:57:29 +0200

    Prueba traduccion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
+import Translator from "./translator";
+
+var translator = new Translator({
+    persist: false,
+    languages: ["en", "es"],
+    defaultLanguage: "en",
+    detectLanguage: true,
+    filesLocation: "/i18n"
+  });
+  
+translator.load();
+
+document.querySelector("form").addEventListener("click", function(evt) {
+    if (evt.target.tagName === "INPUT") {
+      translator.load(evt.target.value);
+    }
+  });
+
 jQuery(document).ready(function($) {
-
-
     /*======= Skillset *=======*/
-    
-    
     $('.level-bar-inner').css('width', '0');
     
     $(window).on('load', function() {
