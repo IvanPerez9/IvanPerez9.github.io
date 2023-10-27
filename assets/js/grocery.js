e676c84a5c9a4b4e34247bbfa92f1ca36ec35056
IvanPerez9 <ivan120695@gmail.com>
2023-10-27 23:12:57 +0200

    Init groceryList

---
+++ assets/js/grocery.js
@@ -0,0 +1,62 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+/*
+Funcion que controla el añadido a la lista
+*/
+function addTask(){
+    // Si no he escrito nada, no hacer nada
+    if(inputBox.value === ''){
+        
+    } else {
+        // Crea el elemento de la lista
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        // Le añade una cruz para el borrado
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+
+    // Limpiar la entrada despues de añadir
+    inputBox.value = "";
+    // Almacena lo añadido
+    saveData();
+}
+
+/*
+Funciona para escuchar distintos click en la app
+*/
+listContainer.addEventListener("click", function(e){
+    // Mira si hacemos click en un LI
+    // Cambiamos o quitamos el checked
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    // Si hacemos click en un SPAN
+    // Borra el elemento
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+} , false);
+
+/*
+Funciona para almacenar lo escrito en sesiones
+Lo guardamos en local como "data"
+*/
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+/*
+Funcion para ver siempre "data" cuando recargemos la web
+*/
+function showDataList(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+// Llamada para recuperar siempre los datos
+showDataList();
