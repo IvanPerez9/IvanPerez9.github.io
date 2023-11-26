0996c689b68102049ebfda3d1e41f8b3cdaf83ec
IvanPerez9 <ivan120695@gmail.com>
2023-11-26 20:09:10 +0100

    Drag and drop

--- assets/js/weekly.js
+++ assets/js/weekly.js
@@ -1,98 +1,161 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   const daysContainer = document.getElementById("days-container");
-  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
   const burgerMenu = document.getElementById("burgerMenu");
 
-  // Load saved meals from local storage
+  // Cargar comidas guardadas desde el almacenamiento local
   const savedMeals = JSON.parse(localStorage.getItem("weeklyMeals")) || {};
-  // Burger menu button
+  // Botón del menú hamburguesa
   const burgerBtn = document.getElementById("burgerBtn");
   burgerBtn.addEventListener("click", toggleBurgerMenu);
 
   function toggleBurgerMenu() {
-    burgerMenu.classList.toggle("show");
+      burgerMenu.classList.toggle("show");
   }
 
-  // Load elements from JSON and populate the burger menu
+  // Cargar elementos desde JSON y poblar el menú hamburguesa
   fetch('assets/json/meals.json')
-    .then(response => response.json())
-    .then(data => {
-      populateBurgerMenu(data);
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
+      .then(response => response.json())
+      .then(data => {
+          populateBurgerMenu(data);
+      })
+      .catch(error => console.error('Error al obtener JSON:', error));
 
   function populateBurgerMenu(data) {
-    // Clear previous content
-    burgerMenu.innerHTML = "";
+      // Limpiar el contenido previo
+      burgerMenu.innerHTML = "";
 
-    // Create and append list items for lunch
-    const lunchList = document.createElement("ul");
-    const lunchItems = data.lunch || [];
-    lunchItems.forEach(item => {
-      const listItem = document.createElement("li");
-      listItem.textContent = item;
-      lunchList.appendChild(listItem);
-    });
+      // Crear y agregar elementos de la lista para el almuerzo
+      const lunchList = createList(data.lunch || [], "lunch-list");
+      burgerMenu.appendChild(lunchList);
 
-    // Create and append list items for dinner
-    const dinnerList = document.createElement("ul");
-    const dinnerItems = data.dinner || [];
-    dinnerItems.forEach(item => {
-      const listItem = document.createElement("li");
-      listItem.textContent = item;
-      dinnerList.appendChild(listItem);
-    });
+      // Crear y agregar elementos de la lista para la cena
+      const dinnerList = createList(data.dinner || [], "dinner-list");
+      burgerMenu.appendChild(dinnerList);
+  }
 
-    // Append lunch and dinner lists to the burger menu
-    burgerMenu.appendChild(lunchList);
-    burgerMenu.appendChild(dinnerList);
+  function createList(items, listId) {
+      const list = document.createElement("ul");
+      list.id = listId;
+
+      items.forEach(item => {
+          const listItem = document.createElement("li");
+          listItem.textContent = item;
+          listItem.draggable = true; // Asegurarse de que sea arrastrable
+          listItem.addEventListener("dragstart", handleDragStart);
+          listItem.addEventListener("dragend", handleDragEnd);
+
+          list.appendChild(listItem);
+      });
+
+      return list;
   }
 
   daysOfWeek.forEach(day => {
-    const dayDiv = document.createElement("div");
-    dayDiv.classList.add("day");
+      const dayDiv = document.createElement("div");
+      dayDiv.classList.add("day");
 
-    const dayLabel = document.createElement("div");
-    dayLabel.classList.add("day-label");
-    dayLabel.textContent = day;
+      const dayLabel = document.createElement("div");
+      dayLabel.classList.add("day-label");
+      dayLabel.textContent = day;
 
-    const lunchInput = createMealInput("Lunch", day);
-    const dinnerInput = createMealInput("Dinner", day);
+      const lunchInput = createMealInput("Comida", day);
+      const dinnerInput = createMealInput("Cena", day);
 
-    // Load saved meals if available
-    lunchInput.value = savedMeals[`${day}-Lunch`] || "";
-    dinnerInput.value = savedMeals[`${day}-Dinner`] || "";
+      // Cargar comidas guardadas si están disponibles
+      lunchInput.value = savedMeals[`${day}-Lunch`] || "";
+      dinnerInput.value = savedMeals[`${day}-Dinner`] || "";
 
-    dayDiv.appendChild(dayLabel);
-    dayDiv.appendChild(lunchInput);
-    dayDiv.appendChild(dinnerInput);
+      dayDiv.appendChild(dayLabel);
+      dayDiv.appendChild(lunchInput);
+      dayDiv.appendChild(dinnerInput);
 
-    daysContainer.appendChild(dayDiv);
+      daysContainer.appendChild(dayDiv);
   });
 
   function createMealInput(mealType, day) {
-    const mealInput = document.createElement("input");
-    mealInput.classList.add("meal-input");
-    mealInput.placeholder = `Enter ${mealType.toLowerCase()} for ${day}`;
-    mealInput.addEventListener("input", function () {
-      // Save the entered meal to local storage
-      savedMeals[`${day}-${mealType}`] = mealInput.value;
-      localStorage.setItem("weeklyMeals", JSON.stringify(savedMeals));
-    });
-    return mealInput;
+      const mealInput = document.createElement("input");
+      mealInput.classList.add("meal-input");
+      mealInput.placeholder = `Ingresa ${mealType.toLowerCase()} para ${day}`;
+
+      // Cargar comidas guardadas si están disponibles
+      mealInput.value = savedMeals[`${day}-${mealType}`] || "";
+
+      // Event listener para la entrada manual
+      mealInput.addEventListener("input", function() {
+          // Guardar la comida ingresada en el almacenamiento local
+          savedMeals[`${day}-${mealType}`] = mealInput.value;
+          localStorage.setItem("weeklyMeals", JSON.stringify(savedMeals));
+      });
+
+      // Event listener para elementos arrastrados
+      mealInput.addEventListener("drop", function(event) {
+          event.preventDefault();
+          const draggedItemText = event.dataTransfer.getData("text/plain");
+          mealInput.value = draggedItemText;
+
+          // Guardar la comida ingresada en el almacenamiento local
+          savedMeals[`${day}-${mealType}`] = mealInput.value;
+          localStorage.setItem("weeklyMeals", JSON.stringify(savedMeals));
+      });
+
+      // Evitar que el campo de entrada maneje el evento de soltar por defecto
+      mealInput.addEventListener("dragover", function(event) {
+          event.preventDefault();
+      });
+
+      return mealInput;
   }
 
-  // Clear all data button
-  const clearDataBtn = document.getElementById("clearDataBtn");
-  clearDataBtn.addEventListener("click", function () {
-    localStorage.removeItem("weeklyMeals");
-    // Reset input values
-    document.querySelectorAll(".meal-input").forEach(input => input.value = "");
+  // Función para manejar el evento de inicio de arrastre
+  function handleDragStart(event) {
+      event.dataTransfer.setData("text/plain", event.target.textContent);
+  }
+
+  // Función para manejar el evento de fin de arrastre
+  function handleDragEnd(event) {
+      // Agregar cualquier código de limpieza necesario aquí
+  }
+
+  // Función para manejar el evento de soltar
+  function handleDrop(event) {
+      event.preventDefault();
+      const draggedItemText = event.dataTransfer.getData("text/plain");
+
+      // Identificar el campo de entrada de destino
+      const targetInput = document.activeElement;
+
+      // Verificar si el elemento activo es un campo de entrada
+      if (targetInput.tagName === 'INPUT' && targetInput.classList.contains('meal-input')) {
+          // Establecer el valor del campo de entrada al texto arrastrado
+          targetInput.value = draggedItemText;
+
+          // Guardar la comida ingresada en el almacenamiento local
+          const day = targetInput.getAttribute('data-day');
+          const mealType = targetInput.getAttribute('data-meal-type');
+          savedMeals[`${day}-${mealType}`] = targetInput.value;
+          localStorage.setItem("weeklyMeals", JSON.stringify(savedMeals));
+      }
+  }
+
+  // Agregar escuchadores de eventos para arrastrar y soltar
+  mealPlanner.addEventListener("dragover", function(event) {
+      event.preventDefault(); // Permitir soltar
   });
 
-  // Go to other page button
+  mealPlanner.addEventListener("drop", handleDrop);
+
+  // Botón para borrar todos los datos
+  const clearDataBtn = document.getElementById("clearDataBtn");
+  clearDataBtn.addEventListener("click", function() {
+      localStorage.removeItem("weeklyMeals");
+      // Restablecer los valores de entrada
+      document.querySelectorAll(".meal-input").forEach(input => input.value = "");
+  });
+
+  // Botón para ir a otra página
   function goToOtherPage() {
-    // You can replace this with the URL of the other page
-    window.location.href = "otherpage.html";
+      // Puedes reemplazar esto con la URL de la otra página
+      window.location.href = "otrapagina.html";
   }
 });
